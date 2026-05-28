606804a3aed57388d3c7befe5d3f8a0af15f8e30
Anna Semina <anna290303@yandex.ru>
2026-05-28 16:35:42 +0300

    some changes

---
+++ backend/auth_service/sql/change_password.py
@@ -0,0 +1,11 @@
+import bcrypt
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(password=pwd_bytes, salt=salt)
+
+# hashed = hash_password("9876543211")
+hashed = hash_password("9998887766")
+print(hashed) # байтовая строка, например b'$2b$12$...'
+hex_str = "\\\\x" + hashed.hex()
+print(hex_str)
